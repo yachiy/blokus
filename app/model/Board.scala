cb456f1c4305e4beb933638331eda19d5db20718
k-yachida <k-yachida@atware.co.jp>
2019-02-04 22:44:22 +0900

    acceptをリファクタ

--- app/model/Board.scala
+++ app/model/Board.scala
@@ -3,23 +3,17 @@ package model
 class Board(val row: Int, val column: Int) {
   private val cells: Array[Array[Int]] = Array.ofDim(row, column)
 
-  // FIXME 良い書き方教えてください
   def accept(playerId: Int, leftTop: Point, block: Block): Boolean = {
     require(leftTop.x >= 0 && leftTop.y >= 0)
     require(leftTop.x + block.column < column && leftTop.y + block.row < row)
 
     val pointsOnBoard = block.points.map(_ + leftTop)
-    if (pointsOnBoard.exists(valueOf(_) != 0)) {
-      return false
+    if (isValid(pointsOnBoard, playerId)) {
+      pointsOnBoard.foreach(put(playerId, _))
+      true
+    } else {
+      false
     }
-    if (pointsOnBoard.exists(hasAdjoinedCells(playerId, _))) {
-      return false
-    }
-    if (!pointsOnBoard.exists(touchedOnCorner(playerId, _))) {
-      return false
-    }
-    pointsOnBoard.foreach(put(playerId, _))
-    true
   }
 
   def in(point: Point): Boolean = point.x >= 0 && point.y >= 0 && point.x < column && point.y < row
@@ -30,13 +24,17 @@ class Board(val row: Int, val column: Int) {
     cells(point.x)(point.y) = playerId
   }
 
-  def hasAdjoinedCells(playerId: Int, point: Point): Boolean = {
+  private def isValid(points: Seq[Point], playerId: Int): Boolean = {
+    !points.exists(valueOf(_) != 0) && !points.exists(hasAdjoinedCells(playerId, _)) && points.exists(touchedOnCorner(playerId, _))
+  }
+
+  private def hasAdjoinedCells(playerId: Int, point: Point): Boolean = {
     Seq(point.left(), point.right(), point.up(), point.down())
       .filter(in)
       .exists(valueOf(_) == playerId)
   }
 
-  def touchedOnCorner(playerId: Int, point: Point): Boolean = {
+  private def touchedOnCorner(playerId: Int, point: Point): Boolean = {
     Seq(point.left().up(), point.left().down(), point.right().up(), point.right().down())
       .filter(in)
       .exists(valueOf(_) == playerId)
